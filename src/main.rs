4b5fec83e5bf08f6a2b6c6c94d49d15493613c41
keagan erasmus <keaganerasmus12@gmail.com>
2024-01-13 13:22:22 +1300

    Enemies can now be rendered will work on their movement next

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use macroquad::prelude::*;
 
+struct Enemy {
+    pos: Vec2,
+    texture: Texture2D,
+    // enemy_type: str
+}
+
 struct Ship {
     pos: Vec2,
     texture: Texture2D
@@ -13,15 +19,25 @@ struct Bullet {
 async fn main() { 
     let player_speed: f32 = 5.0; 
     let player_texture = Texture2D::from_file_with_format(include_bytes!("Jump.png"), None);
+    let enemy_texture = Texture2D::from_file_with_format(include_bytes!("Jump.png"), None);
+    let mut last_shot = get_time();
+    let fire_rate = 0.5;
     
     let mut ship = Ship {
         pos: Vec2::new(screen_width() / 2.0, 500.0),
         texture: player_texture
     };
-    let mut bullets= Vec::new();
-    let mut last_shot = get_time();
-    let fire_rate = 0.5;
 
+    let mut bullets= Vec::new();
+
+    let mut enemies = Vec::new();
+
+    for i in 1..11{
+        enemies.push(Enemy {
+            pos: Vec2::new(i as f32 * 20.0, 1.0),
+            texture: enemy_texture.clone()
+        });
+    }
     
     loop {
         clear_background(WHITE);
@@ -33,6 +49,11 @@ async fn main() {
         }
         if is_key_down(KeyCode::A) {
             ship.pos.x -= player_speed;
+        }
+
+
+        if is_key_down(KeyCode::P) {
+            // let collected_bullet: Vec<Bullet> = (0..11).collect();
         }
 
         if  is_key_down(KeyCode::Space) && current_time - last_shot > fire_rate{
@@ -52,6 +73,13 @@ async fn main() {
             bullet.pos.y -= 5.0;
         }
 
+        bullets.retain(|bullet| bullet.pos.y > 0.0);
+        
+
+        // Render enemies
+        for enemy in enemies.iter_mut(){
+            draw_texture(&enemy.texture, enemy.pos.x * 2.0, enemy.pos.y, RED);
+        }
 
 
         next_frame().await
