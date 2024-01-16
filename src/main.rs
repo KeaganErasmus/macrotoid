897129e29211439fd9dfc1c751f83b9445a97e02
keagan erasmus <keaganerasmus12@gmail.com>
2024-01-16 13:40:52 +1300

    formatted code a little bit more

--- src/main.rs
+++ src/main.rs
@@ -75,76 +75,64 @@ async fn main() {
         clear_background(WHITE);
         let score_text = format!("{}", score);
 
-        draw_text(&score_text, 50.0, 50.0, 50.0, BLACK);
+        draw_text(&score_text, screen_width() / 2.0, 50.0, 50.0, BLACK);
 
         let current_time = get_time();
 
         if !ship.is_dead {
             draw_texture(&ship.texture, ship.pos.x, ship.pos.y, WHITE);
-        }
 
-        if current_time - last_spawn > spawn_rate {
-            enemies.push(Enemy::new(
-                width_spawn as f32,
-                height_spawn as f32,
-                enemy_texture.clone(),
-                5,
-            ));
-            last_spawn = current_time;
-        }
-
-        if is_key_down(KeyCode::D) {
-            ship.pos.x += player_speed;
-        }
-        if is_key_down(KeyCode::A) {
-            ship.pos.x -= player_speed;
-        }
-
-        if ship.pos.x >= screen_width() - ship.texture.width() {
-            ship.pos.x = screen_width() - ship.texture.width();
-        }
-        if ship.pos.x <= 0.0 {
-            ship.pos.x = 0.0;
-        }
-
-        if is_key_down(KeyCode::Space) && current_time - last_shot > fire_rate {
-            bullets.push(Bullet {
-                pos: ship.pos,
-                is_active: true,
-                collision_rect: Rect::new(ship.pos.x, ship.pos.y, 5.0, 5.0),
-            });
-            last_shot = current_time;
-        }
-
-        if is_key_down(KeyCode::L) && current_time - last_shot > fire_rate {
-            enemies.push(Enemy::new(
-                width_spawn as f32,
-                height_spawn as f32,
-                enemy_texture.clone(),
-                5,
-            ));
-            last_shot = current_time;
-        }
-
-        // Render Bullets
-        for bullet in bullets.iter_mut() {
-            if bullet.is_active {
-                draw_rectangle(bullet.pos.x, bullet.pos.y, 5.0, 5.0, RED);
+            if is_key_down(KeyCode::D) {
+                ship.pos.x += player_speed;
             }
-        }
-
-        // Move Bullets
-        for bullet in bullets.iter_mut() {
-            if bullet.is_active {
-                bullet.pos.y -= 5.0;
+            if is_key_down(KeyCode::A) {
+                ship.pos.x -= player_speed;
             }
 
-            bullet.collision_rect.x = bullet.pos.x;
-            bullet.collision_rect.y = bullet.pos.y;
-        }
+            if ship.pos.x >= screen_width() - ship.texture.width() {
+                ship.pos.x = screen_width() - ship.texture.width();
+            }
+            if ship.pos.x <= 0.0 {
+                ship.pos.x = 0.0;
+            }
 
-        // Render enemies
-        if !ship.is_dead {
+            if is_key_down(KeyCode::Space) && current_time - last_shot > fire_rate {
+                bullets.push(Bullet {
+                    pos: ship.pos,
+                    is_active: true,
+                    collision_rect: Rect::new(ship.pos.x, ship.pos.y, 5.0, 5.0),
+                });
+                last_shot = current_time;
+            }
+
+            // Render Bullets
+            for bullet in bullets.iter_mut() {
+                if bullet.is_active {
+                    draw_rectangle(bullet.pos.x, bullet.pos.y, 5.0, 5.0, RED);
+                }
+            }
+
+            // Move Bullets
+            for bullet in bullets.iter_mut() {
+                if bullet.is_active {
+                    bullet.pos.y -= 5.0;
+                }
+
+                bullet.collision_rect.x = bullet.pos.x;
+                bullet.collision_rect.y = bullet.pos.y;
+            }
+
+            if current_time - last_spawn > spawn_rate {
+                enemies.push(Enemy::new(
+                    width_spawn as f32,
+                    height_spawn as f32,
+                    enemy_texture.clone(),
+                    5,
+                ));
+                last_spawn = current_time;
+            }
+
+            // Render enemies
             for enemy in enemies.iter_mut() {
                 draw_texture(&enemy.texture, enemy.pos.x, enemy.pos.y, RED);
             }
@@ -180,14 +168,13 @@ async fn main() {
             if ship.health <= 0 {
                 ship.is_dead = true;
             }
-        }
-
-        // Checks for bullet and enemy collsisions
-        for enemy in enemies.iter_mut() {
-            for bullet in bullets.iter_mut() {
-                if bullet.collision_rect.overlaps(&enemy.collision_rect) {
-                    bullet.is_active = false;
-                    enemy.health -= 5;
+            // Checks for bullet and enemy collsisions
+            for enemy in enemies.iter_mut() {
+                for bullet in bullets.iter_mut() {
+                    if bullet.collision_rect.overlaps(&enemy.collision_rect) {
+                        bullet.is_active = false;
+                        enemy.health -= 5;
+                    }
                 }
             }
         }
